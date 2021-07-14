3eecd1e4c651a1cb1c7d3e173ec5b3813a643cef
xuzhou <xuzhou@shindata.com>
2021-07-15 07:34:59 +0800

    week4 homework

---
+++ src/main/java/com/algo/xuz/week4/design_twitter.java
@@ -0,0 +1,133 @@
+package com.algo.xuz.week4;
+
+import java.util.*;
+
+/**
+ * design_twitter
+ *
+ * 设计一个简化版的推特(Twitter)，可以让用户实现发送推文，关注/取消关注其他用户，能够看见关注人（包括自己）的最近十条推文。你的设计需要支持以下的几个功能：
+ *
+ * postTweet(userId, tweetId): 创建一条新的推文
+ * getNewsFeed(userId): 检索最近的十条推文。每个推文都必须是由此用户关注的人或者是用户自己发出的。推文必须按照时间顺序由最近的开始排序。
+ * follow(followerId, followeeId): 关注一个用户
+ * unfollow(followerId, followeeId): 取消关注一个用户
+ * 示例:
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/design-twitter
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * @author 18736
+ * @version 1.0
+ * 2021/7/15 7:29
+ **/
+public class design_twitter {
+
+    private class Node {
+        // 哈希表存储关注人的 Id
+        Set<Integer> followee;
+        // 用链表存储 tweetId
+        LinkedList<Integer> tweet;
+
+        Node() {
+            followee = new HashSet<Integer>();
+            tweet = new LinkedList<Integer>();
+        }
+    }
+
+    // getNewsFeed 检索的推文的上限以及 tweetId 的时间戳
+    private int recentMax, time;
+    // tweetId 对应发送的时间
+    private Map<Integer, Integer> tweetTime;
+    // 每个用户存储的信息
+    private Map<Integer, Node> user;
+
+    public design_twitter() {
+        time = 0;
+        recentMax = 10;
+        tweetTime = new HashMap<Integer, Integer>();
+        user = new HashMap<Integer, Node>();
+    }
+
+    // 初始化
+    public void init(int userId) {
+        user.put(userId, new Node());
+    }
+
+    public void postTweet(int userId, int tweetId) {
+        if (!user.containsKey(userId)) {
+            init(userId);
+        }
+        // 达到限制，剔除链表末尾元素
+        if (user.get(userId).tweet.size() == recentMax) {
+            user.get(userId).tweet.remove(recentMax - 1);
+        }
+        user.get(userId).tweet.addFirst(tweetId);
+        tweetTime.put(tweetId, ++time);
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+        LinkedList<Integer> ans = new LinkedList<Integer>();
+        for (int it : user.getOrDefault(userId, new Node()).tweet) {
+            ans.addLast(it);
+        }
+        for (int followeeId : user.getOrDefault(userId, new Node()).followee) {
+            if (followeeId == userId) { // 可能出现自己关注自己的情况
+                continue;
+            }
+            LinkedList<Integer> res = new LinkedList<Integer>();
+            int tweetSize = user.get(followeeId).tweet.size();
+            Iterator<Integer> it = user.get(followeeId).tweet.iterator();
+            int i = 0;
+            int j = 0;
+            int curr = -1;
+            // 线性归并
+            if (j < tweetSize) {
+                curr = it.next();
+                while (i < ans.size() && j < tweetSize) {
+                    if (tweetTime.get(curr) > tweetTime.get(ans.get(i))) {
+                        res.addLast(curr);
+                        ++j;
+                        if (it.hasNext()) {
+                            curr = it.next();
+                        }
+                    } else {
+                        res.addLast(ans.get(i));
+                        ++i;
+                    }
+                    // 已经找到这两个链表合起来后最近的 recentMax 条推文
+                    if (res.size() == recentMax) {
+                        break;
+                    }
+                }
+            }
+            for (; i < ans.size() && res.size() < recentMax; ++i) {
+                res.addLast(ans.get(i));
+            }
+            if (j < tweetSize && res.size() < recentMax) {
+                res.addLast(curr);
+                for (; it.hasNext() && res.size() < recentMax;) {
+                    res.addLast(it.next());
+                }
+            }
+            ans = new LinkedList<Integer>(res);
+        }
+        return ans;
+    }
+
+    public void follow(int followerId, int followeeId) {
+        if (!user.containsKey(followerId)) {
+            init(followerId);
+        }
+        if (!user.containsKey(followeeId)) {
+            init(followeeId);
+        }
+        user.get(followerId).followee.add(followeeId);
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+        user.getOrDefault(followerId, new Node()).followee.remove(followeeId);
+    }
+
+}
