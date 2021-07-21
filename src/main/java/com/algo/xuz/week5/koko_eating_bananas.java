a209cba68dc6f5351ba1e5e8c96c0d1fd8a67f60
xuzhou <xuzhou@shindata.com>
2021-07-21 22:51:53 +0800

    week5(homework)

---
+++ src/main/java/com/algo/xuz/week5/koko_eating_bananas.java
@@ -0,0 +1,45 @@
+package com.algo.xuz.week5;
+
+/**
+ * koko_eating_bananas
+ * <p>
+ * 875. 爱吃香蕉的珂珂
+ * 珂珂喜欢吃香蕉。这里有 N 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 H 小时后回来。
+ * <p>
+ * 珂珂可以决定她吃香蕉的速度 K （单位：根/小时）。每个小时，她将会选择一堆香蕉，从中吃掉 K 根。如果这堆香蕉少于 K 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
+ * <p>
+ * 珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/koko-eating-bananas
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author 18736
+ * @version 1.0
+ * 2021/7/21 22:50
+ **/
+public class koko_eating_bananas {
+
+    public int minEatingSpeed(int[] piles, int H) {
+        int lo = 1;
+        int hi = 1_000_000_000;
+        while (lo < hi) {
+            int mi = (lo + hi) / 2;
+            if (!possible(piles, H, mi))
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+
+        return lo;
+    }
+
+    // Can Koko eat all bananas in H hours with eating speed K?
+    public boolean possible(int[] piles, int H, int K) {
+        int time = 0;
+        for (int p : piles)
+            time += (p - 1) / K + 1;
+        return time <= H;
+    }
+
+}
